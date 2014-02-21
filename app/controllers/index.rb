a7306bd1c6117659a84b5a1aa713da5cc8a7f7a8
dandydanny <dandydanny@gmail.com>
2014-02-21 09:20:36 -0600

    Implement index form and view

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -1,4 +1,6 @@
 get '/' do
   # Look in app/views/index.erb
+  # list a bunch of blog posts
+  @all_posts = Post.all
   erb :index
 end
--- app/controllers/posts.rb
+++ app/controllers/posts.rb
@@ -1,4 +1,17 @@
-post '/post' do
-  # Look in app/views/index.erb
-  # erb :index
+post '/write' do
+  # save post to database
+  p params
+
+  p = Post.create(title: params[:title], body: params[:body])
+  bunch_o_tags = params[:tags].split(',')
+  # puts bunch_o_tags.class
+  # puts "p is..."
+  # p.inspect
+  bunch_o_tags.each do |singletag|
+    p.tags << Tag.create(tag: singletag)
+  end
+  @all_posts = Post.all
+
+  "done"
+
 end
